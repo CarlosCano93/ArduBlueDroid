d2a5193010c54467f25448fd3bf4db8d0c147fe1
CarlosCano93 <carlcan.93@gmail.com>
2016-11-12 19:50:52 +0100

    Add Arduino project to run with Android app

---
+++ ArduBlueDroid.ino
@@ -0,0 +1,52 @@
+char command;
+String string;
+boolean ledon = false;
+int led1 = 5;
+
+  void setup(){
+    Serial.begin(9600);
+    pinMode(led1, OUTPUT);
+  }
+
+  void loop(){
+    if (Serial.available() > 0) {
+      string = "";
+    }
+
+
+    // the message recibed from android is sended char by char
+    while(Serial.available() > 0) {
+      command = ((byte)Serial.read()); // read from bluetooth (or MonitorSerie)
+      
+      if(command == ':'){ // indicate the end of the message
+        break;
+      } else {
+        string += command; // put message together
+      }
+      
+      delay(1); // to prevent bugs
+    }
+
+    // actions to do when message is correct
+    if(string == "L1"){ 
+        ledOn(led1);
+    }
+    
+    if(string =="L2"){
+        ledOff(led1);
+    }
+ }
+ 
+void ledOn(int led){
+      digitalWrite(led, HIGH);
+      delay(10);
+    }
+ 
+ void ledOff(int led){
+      digitalWrite(led, LOW);
+      delay(10);
+ }
+ 
+
+    
+
