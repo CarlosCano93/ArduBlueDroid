cac98d75f8d18fe61c3810c8ebd5824eb728789d
Carlos Cano <carlcan.93@gmail.com>
2016-11-12 12:14:37 +0100

    Paired devices list added and change Butterknife version to 7 because I have problem with 8

--- app/src/main/java/ccano/android/ardubluedroid/MainActivity.java
+++ app/src/main/java/ccano/android/ardubluedroid/MainActivity.java
@@ -4,10 +4,9 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 import android.widget.Toast;
 
-import butterknife.Bind;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
@@ -19,36 +18,46 @@ public class MainActivity extends AppCompatActivity {
     //used to turn on bluetooth
     private static final int REQUEST_ENABLE_BT = 1;
 
-    //See ButterKnife framework to understand @Bind @OnClick and more..
-    @Bind(R.id.btOn)
-    Button btOn;
-    @Bind(R.id.btOff)
-    Button btOff;
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        //To check compatibility and turn on bluetooth
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        activateBluetooth();
+    }
+
+    //open new activity with device bluetooth list
+    @OnClick(R.id.btSearchDevices)
+    public void handleButtonSearchDevice(){
+        Intent intentDeviceList = new Intent(getApplicationContext(), BluetoothDevicesActivity.class);
+        startActivity(intentDeviceList);
+    }
+
+    @OnClick(R.id.btOn)
+    void handleButtonOn(){
+        //TODO send info to put led on
+        Toast.makeText(this, "LED ON", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.btOff)
+    void handleButtonOff(){
+        //TODO send info to put led Off
+        Toast.makeText(this, "LED OFF", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Check bluetooth compatibility
+     * Turn on bluetooth
+     */
+    public void activateBluetooth(){
         if (mBluetoothAdapter == null) {
             Toast.makeText(this, getString(R.string.bluetooth_incompatible), Toast.LENGTH_SHORT).show();
         } else if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
-
-
-    }
-
-    @OnClick(R.id.btOn)
-    public void handleButtonOn(){
-        //TODO send info to put led on
-    }
-
-    @OnClick(R.id.btOff)
-    public void handleButtonOff(){
-        //TODO send info to put led Off
     }
 }
